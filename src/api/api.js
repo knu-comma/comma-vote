a62ee263be2bc187f251af11be91ab6a409f1e6d
cskhw <gusdn0828@gmail.com>
2021-02-13 01:26:59 +0900

    change baseURL to aws

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/"
+    baseURL: "http://comma-api.eba-2tnypy8y.ap-northeast-2.elasticbeanstalk.com/"
 });
 
 export default instance;
