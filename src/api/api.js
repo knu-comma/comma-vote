f95412e082921fbe0a7b5fe979035191e6efbdef
cskhw <51100329+cskhw@users.noreply.github.com>
2021-02-14 22:23:49 +0900

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
     // baseURL: "http://comma-api.eba-2tnypy8y.ap-northeast-2.elasticbeanstalk.com/"
-    baseURL: "http://localhost:3000/"
+    baseURL: "https://121.158.146.119:443/"
 });
 
 export default instance;
